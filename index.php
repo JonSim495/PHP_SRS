ec169bd0c4723c271184222bf2887b0a75f6db1c
Tan Zhong Yan <iam@tzhongyan.com>
2016-09-21 14:33:13 +0800

    First index.php, to be either edited or deleted in the future

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>People's Health Pharmacy - Sales Reporting System</title>
+</head>
+
+<body>
+    <h1>Hello world!</h1>
+    <p>This is the first page that we are having ffs.</p>
+    <?php
+      echo "<hr /><p>Hope this is a good one</p>";
+    ?>
+</body>
+
+</html>
