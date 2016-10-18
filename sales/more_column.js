b958ed94f57248369db279809a0fc2ef80420081
Tan Zhong Yan <tzhongyan@tzhongyan.com>
2016-10-18 14:40:40 +0800

    Fixes form reset after adding more item.

--- sales/more_column.js
+++ sales/more_column.js
@@ -11,21 +11,48 @@ xmlhttp.onreadystatechange = function(){
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-var count=0;
+var count=1;
 function more(arr){
-    var out = "<br />Item " + count++ + ": <select name=\"item[][id]\" form=\"sales\" required>";
-    out += "<option value=\"\">Please Select</option>";
-	for (var i=0; i<arr.length; i++){
-	  out+="<option value=\"" + arr[i].itemID + "\">" + arr[i].itemName + "</option>";
-	}
+    var theForm = document.getElementById("forms");
 
-    out+="</select><br />"
-	out+="Amount bought: <input type=\"number\" name=\"item[][count]\" min=\"1\" required><br />"
-document.getElementById("forms").innerHTML+=out;
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode('Item '+ count++ + ': '));
+
+    var select = document.createElement("select");
+    select.name = "id[]";
+    select.form = "sales";
+    select.required = "required";
+
+	var opt = document.createElement("option");
+	opt.appendChild(document.createTextNode("Please select"));
+	opt.value = "";
+	select.appendChild(opt);
+
+    for (var i=0; i<arr.length; i++){
+        var option = document.createElement("option");
+		option.appendChild(document.createTextNode(arr[i].itemName));
+        option.value = arr[i].itemID;
+        select.appendChild(option);
+    }
+
+    div.appendChild(select);
+
+    var div2 = document.createElement("div");
+    div2.appendChild(document.createTextNode('Amount bought: '))
+
+    var num = document.createElement("INPUT");
+    num.setAttribute("type", "number");
+    num.value = "1";
+    num.min = "1";
+    num.name = "count[]";
+
+    div2.appendChild(num);
+
+    theForm.appendChild(div);
+    theForm.appendChild(div2);
 }
 
 function moreItem(){
     more(myArr);
 }
 
-window.onload = moreItem;
