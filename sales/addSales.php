d22a4faeb81d3dd2a3f3c2d7453cdc9b3290478e
Tan Zhong Yan <tzhongyan@tzhongyan.com>
2016-10-19 17:16:03 +0800

    Finally... A working add sales php file

--- sales/addSales.php
+++ sales/addSales.php
@@ -12,6 +12,7 @@
     	$date = $_POST["date"];
     	$itemID = $_POST["id"];
 		$itemCount = $_POST["count"];
+		$count = count($itemID);
 
     	// authentication to the database
     	$servername = "localhost";
@@ -25,43 +26,43 @@
     	if (!$conn) {
     	    die("Connection failed: " . $conn->connect_error);
     	}
+		
+		// select database
+		mysqli_select_db($conn, "swe30010");
 
     	//Get next sales ID from auto increment
-    	$result = mysqli_query($conn, "USE swe30010;SHOW TABLE STATUS WHERE name='Sales';");
+		$sql = "SHOW TABLE STATUS WHERE `Name` = 'Sales';";
+    	$result = mysqli_query($conn, $sql);
     	$data = mysqli_fetch_assoc($result);
-
-		echo "data=".$data."<br />";
 
     	$salesID = $data['Auto_increment'];
 		
-		echo "salesID=".$salesID."<br />";
-
     	// Add into Sales table
-    	$sql = "INSERT INTO `swe30010`.`Sales` (salesID, salesDate, invoice) VALUES($salesID, '$date', '$invoice')";
-		echo $sql;
+    	$sql = "INSERT INTO `Sales` (salesID, salesDate, invoice) VALUES($salesID, '$date', '$invoice')";
 
-/*
-      $if (mysqli_query($conn, $sql)) {
-    	    echo "<h1>Record added into Sales table.</h1>";
+        if (mysqli_query($conn, $sql)) {
+            //echo "<h1>Record added into Sales table.</h1>";
     	} else {
     	    echo "Error: $sql <br />" . mysqli_error($conn);
     	}
 
-
     	//Add item into Sales Item table
-    	foreach($i as $item){
-    	    //do sth?
-    	    $itemID = $i["id"];
-    	    $itemCount = $i["count"];
-    	    $sql = "INSERT INTO `swe30010`.`SalesItem` (salesID, itemID, itemCount)
-    	            VALUES ($salesID, $itemID, $itemCount)";
+		for ($i=0; $i<$count; $i++){
+			$sql = "INSERT INTO SalesItem (salesID, itemID, itemCount) VALUES ($salesID, '$itemID[$i]', $itemCount[$i])";
 
-          $if (mysqli_query($conn, $sql)){
-          echo "<p>Item $itemID added into Sales table.</p>";
-    	    }
-    	}
-*/
+			if (mysqli_query($conn, $sql)){
+				//echo "<p>Item $itemID[$i] has been added to $salesID</p>";
+			}
+		}
         mysqli_close($conn);
+		echo '<div>';
+		echo "<script>
+				window.alert('Record added successfully!');
+				window.location.href = 'showSales.html';
+		</script>
+		";
+		echo "</div>"
+
     ?>
   </body>
 
