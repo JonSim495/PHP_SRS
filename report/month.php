4c129ebd25b6014f77dc644c69e940fa8356527d
Tan Zhong Yan <tzhongyan@tzhongyan.com>
2016-10-25 21:59:38 +0800

    Show months available done

--- report/month.php
+++ report/month.php
@@ -3,13 +3,43 @@
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
 
-    $date = $_POST["date"];
+    $date = $_GET["date"];
+
+    $servername = "localhost";
+    $username = "";
+    $password = "";
+    $conn = mysqli_connect($servername, $username, $password);
+
     if ($date == ""){
-        // TODO: when date is not available, show the ajax for month report available
-        echo "[]";    
+        // when date is empty, show the ajax for month report available
+        if (!$conn){
+            // no connection
+            echo "[]";
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            $sql = "SELECT DISTINCT MONTH(salesDate) AS month, YEAR(salesDate) AS year FROM `swe30010`.`Sales`";
+            $result = mysqli_query($conn, $sql);
+
+            // start of json
+            $output = "[";
+            while ($row = mysqli_fetch_assoc($result)){
+                if ($output != "["){
+                    $output .= ",";
+                }
+
+                // dates available
+                $output .= '{"date":"' . $row["year"] . '-' . $row["month"] . '"}';
+            }
+            // end of json
+            $output .= "]";
+            echo $output;
+           
+        }
     } else {
         // TODO: when date is available, show the sales of the month
         echo "[]";
     }
+
+    mysqli_close($conn);
 
 ?>
--- report/selectMonth.js
+++ report/selectMonth.js
@@ -1,45 +1,95 @@
 "use strict";
 
+/*
+*   Input parameter: arr - array parsed by json, with parameter 'date'
+*   Output: none
+*
+*   Functionality: Get dates from array arr, put in select option. 
+*/
 function populateSelect(arr){
-    // TODO: month decision 
-    for (int i=0; i<arr.length; i++){
-        var options = document.createElement("option");
-        options.value = arr[i].month; 
-        options.appendChild(document.createTextNode(arr[i].month));
+
+    var options = document.createElement("option");
+    options.value = ""; 
+    options.appendChild(document.createTextNode("Select Month"));
+    document.getElementById("select-month").appendChild(options);
+
+    for (var i=0; i<arr.length; i++){
+        options = document.createElement("option");
+        options.value = arr[i].date; 
+        options.appendChild(document.createTextNode(arr[i].date));
         document.getElementById("select-month").appendChild(options);
     }
+    document.getElementById("select-month").onchange = showData;
 }
 
-function ajaxToJson(date){
+/*
+*   Input parameter: date - input the date for requesting sales data from database 
+*                    callback - inline function to obtain the json for async request
+*   Output: none, use inline function to obtain responseText
+* 
+*   Functionality: Get date, connect to database to obtain data. If date is available,
+*                  return sales within that date. If date string is empty, it shows all 
+*                  dates available for sales reporting.
+*   
+*   Usage: ajaxToJson(date, 
+*       function(){
+*           var arr = JSON.parse(this.responseText);
+*
+*            //do things with arr
+*       }
+*   );
+*
+*    Reference: http://stackoverflow.com/questions/5362462/how-can-i-make-xhr-onreadystatechange-return-its-result
+*/
+function ajaxToJson(date, callback){
     var http = new XMLHttpRequest();
-    var url = "month.php";
-    var params = "date="+date;
-
-    http.open("POST", url, true);
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var url = "month.php?date=" + date;
 
     var demArr;
 
     http.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            demArr = JSON.parse(this.responseText);
-            return demArr;
+        if (http.readyState == 4 && http.status == 200){
+            if(typeof callback == "function"){
+                callback.apply(http);
+            }
         }
     }
+    http.open("GET", url, true);
     http.send();
+
 }
 
+/*
+    Input: none
+    Output: none
+
+    Functionality: Obtain date from select (triggered by onchange), show data from
+                   request in server and populate the tables.
+*/
 function showData(){
     var date = document.getElementById("select-month").value;
-    var arr = ajaxToJson(date);
 
-    var table = document.getElementById("showData");
-    //TODO: show the data of arr in table
+    ajaxToJson(date, 
+        function(){
+            var arr = JSON.parse(this.responseText);
+
+            var salesTable = document.getElementById("showSalesData");
+            var itemTable = document.getElementById("showItemData");
+
+            console.log("//TODO: show the data of arr in table");
+            
+            //TODO: show the data of arr in table
+        }
+    );
 
 }
 
+// Initialisation of progs
 window.onload = function() {
-    var arr = ajaxToJson("");
-    populateSelect(arr);
-    document.getElementById("select-month").onchange = showData;
+    ajaxToJson("", 
+        function(){
+            var arr = JSON.parse(this.responseText);
+            populateSelect(arr);
+        }
+    );
 }
