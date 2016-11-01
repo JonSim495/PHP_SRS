0b6d703def16cae1819b232e90227faf9bba8c23
JonSim495 <master3991>
2016-11-01 11:20:17 +0800

    GUI enhancement

--- inventory/add.php
+++ inventory/add.php
@@ -7,7 +7,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link href="../style/style.css" rel="stylesheet"/>
-        <title>Sales module</title>
+        <title>Add Inventory</title>
 </head>
 
 <body>
@@ -24,10 +24,10 @@
                   <a class="navbar-brand" href="index.html">PHP SRS</a>
                 </div>
                 <ul class="nav navbar-nav">
-                  <li class="active"><a href="index.html">Home</a></li> <!-- Remember to switch to active class on the page you're in -->
-                  <li><a href="about.html">About Us</a></li>
-                  <li><a href="contact.html">Contact Us</a></li>
-                  <li><a href="#"></a></li>
+                  <li><a href="index.html">Home</a></li> <!-- Remember to switch to active class on the page you're in -->
+                  <li class="active"><a href="../inventory/index.html">Inventory</a></li>
+                  <li><a href="../report/index.html">Report</a></li>
+                  <li><a href="../sales/index.html">Sales</a></li>
                 </ul>
               </div>
             </nav>
